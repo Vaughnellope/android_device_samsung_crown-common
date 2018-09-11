3bd466cd6ba01a198239ee22c83fd40285ecacde
Michael Benedict <michaelbt@live.com>
2018-09-11 14:40:46 +1000

    crown: update architecture for pie
    Signed-off-by: Michael Benedict <michaelbt@live.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -20,10 +20,10 @@ TARGET_CPU_VARIANT := cortex-a53
 
 # Secondary Architecture
 TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_ARCH_VARIANT := armv8-a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a53
+TARGET_2ND_CPU_VARIANT := cortex-a55
 
 # Kernel
 TARGET_KERNEL_ARCH := arm64
